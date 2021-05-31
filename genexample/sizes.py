94041dd9e07e5e8905b412a98cc2c671d8b65de4
samuelduwu <60489296+samuelduwu@users.noreply.github.com>
2021-05-31 09:28:02 -0500

    Update sizes.py

--- genexample/sizes.py
+++ genexample/sizes.py
@@ -1,3 +1,4 @@
+#both variables are iterators. big_range is a generator, special type of iterator
 import sys
 
 #defining my own generator
@@ -9,13 +10,14 @@ def my_range(n: int):
       yield start
       start += 1
       
-#yield returns the value but keeps track of prev values nad position
+#yield returns the value but keeps track of prev values and position
       
 pause = input("line 14")
 #big_range = range(1000) # occupies 48 bytes
 big_range = my_range(10000) # occupies 88 bytes 
 pause = input("line 17") # here we see that my_range hasn't been executed
 
+print(next(big_range)) #next gets next val of generator. Shows 0 initially. The next call to the function would yield 1
 print("big_range is {} bytes".format(sys.getsizeof(big_range))) 
 
 pause = input("line 21") #right after this pause, my_range runs
@@ -29,4 +31,7 @@ print("big_list is {} bytes".format(sys.getsizeof(big_list))) # occupies 9024 by
 print(big_range) # this is a generator object
 print(big_list) # this is a list
 
-#both variables are iterators. big_range is a generator, special type of iterator
+print("looping again... or not")
+for i in big_range: #big_range generator is exhausted so nothing runs here. To make it run, change big_range to my_range(5)
+    #if big_range = range(5), we've stored the generator so it can be called multiple times
+    print("i is {}".format(i))
