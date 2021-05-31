1a1085cb5cc160796f22a922c98ee351b484fe0d
samuelduwu <60489296+samuelduwu@users.noreply.github.com>
2021-05-31 09:38:02 -0500

    Create fibgen.py

---
+++ genexample/fibgen.py
@@ -0,0 +1,32 @@
+def fibonacci():
+  current, previous= 0, 1
+  while True:
+    yield current #infinite generator
+    current, previous = current + previous, current
+    
+for f in fib:
+  print(f) #don't do this
+    
+fib = fibonacci()
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
