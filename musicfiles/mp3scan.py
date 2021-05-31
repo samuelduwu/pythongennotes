f74523e0a9e8f3605c72992c6c6794c859c1012e
samuelduwu <60489296+samuelduwu@users.noreply.github.com>
2021-05-31 13:03:11 -0500

    Create mp3scan.py

---
+++ musicfiles/mp3scan.py
@@ -0,0 +1,16 @@
+import os
+import fnmatch
+
+def find_music(start, extension):
+  for path, directories, files in os.walk(start):
+    for file in fnmatch.filter(files, '*.{}'.format(extension)):
+      absolute_path = os.path.abspath(path) # create absolute path
+      yield os.path.join(absolute_path, file) # use it in yielded values
+      
+
+#my_music_files = find_music('music', 'emp3')    
+#for f in my_music_files:
+#  print(f)
+      
+for f in find_music('music', 'emp3'):
+  print(f)
