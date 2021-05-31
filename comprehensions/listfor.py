ab37aa243b90c8f75c37cb51282eb60b26827efb
samuelduwu <60489296+samuelduwu@users.noreply.github.com>
2021-05-31 13:31:25 -0500

    Update listfor.py

--- comprehensions/listfor.py
+++ comprehensions/listfor.py
@@ -3,18 +3,26 @@ print(__file__)
 
 numbers = [1,2,3,4,5,6]
 
+number = int(input("Please enter a number, and I'll tell you its square: "))
+
 squares = []
 for number in numbers:
   squares.append(number**2)
   
-print(squares)
+index_pos = numbers.index(number)
+print(squares[index_pos]))
+
+#since number is used as input and index for for loop, it always outputs 36
 
 # Using comprehensions
 numbers = [1,2,3,4,5,6]
 
-squares = [number ** 2 for number in numbers] #using {} creates a set comprehension
+#squares = [number ** 2 for number in numbers] #using {} creates a set comprehension
+number = int(input("Please enter a number, and I'll tell you its square: "))
 
-print(squares)
+squares = [number ** 2 for number in numbers]
+
+print(squares[index_pos]))
 
 # A list comprehension's first segment (number ** 2) is expression we want to return
 # second is iteration over a sequence. Identical to for part in for loop
